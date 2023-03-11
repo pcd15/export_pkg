490828a2a87a2b2cfd371e5796f34b75af90256f
Paul Dilly <112518601+pcd15@users.noreply.github.com>
2023-03-11 02:29:57 -0500

    Create example.py

---
+++ tests/example.py
@@ -0,0 +1,12 @@
+from exports_from_api.comtrade import Comtrade     # importing the Comtrade class
+from exports_from_api.imf import IMF     # importing the IMF class
+
+comtrade_obj = Comtrade()     # creating a Comtrade object so that we can use the methods of the class we imported
+imf_obj = IMF()     # creating an IMF object so that we can use the methods of the class we imported
+
+# see above documentation
+comtrade_obj.get_all_exports()
+comtrade_obj.get_total_exports()
+
+imf_obj.get_reporter_exports()
+imf_obj.get_total_exports()
